73182d855d3b3b12915f2c0ce1b369ee9625963a
skyler <skyler.nch@gmail.com>
2025-02-07 11:00:29 +0800

    v0.0.2.0

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI,Form
 from dotenv import load_dotenv
+
+from fastapi.middleware.cors import CORSMiddleware
+
+import requests
+
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASEDIR,'.env'))
 
 app = FastAPI()
 
+origins = [
+    "http://172.18.0.1:5000",
+
+    "http://127.0.0.1:9000",
+    "http://127.0.0.1:9001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
-    return {"message":"Hello World","detail":"this is an api gateway/router"}
+    return {"message":"Hello World","detail":"api-gateway"}
 
 @app.get("/{path}")
+async def router(path, payload:str=Form()):
+    response = requests.get(os.getenv("INTERNALROUTERPATH"+"/route"),payload)
+    return {"path":path}
+
+@app.post("/{path}")
 async def router(path):
+    
     return {"path":path}
 
     
